cb098ec0e03e8f31ff4ff184a2e39e6bdb722145
slvamegestic <sselvakumar12@gmail.com>
2020-01-23 15:58:42 +0530

    Add existing file

--- addwork.php
+++ addwork.php
@@ -105,54 +105,41 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
 
     <!-- Right Column -->
     <div class="w3-twothird">
-    
-        <div class="w3-container w3-card w3-white w3-margin-bottom">
-              <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience <a href="addwork.php">
-          <i class="fa fa-plus fa-fw w3-right w3-large w3-text-teal"></i></a></h2>
-              <?php   
-                    $sql= "SELECT * FROM `experience`";
-                    
-                    $result = mysqli_query($conn, $sql);
+    <?php
+      if(isset($_POST['addwork']))
+      {
+          $name =$_POST['p-name'];
+          $duration=$_POST['du-tion'];
+          $discription=$_POST['dis-tion'];
 
-                    while($row = mysqli_fetch_assoc($result)) {
-              ?>
-              <div class="w3-container">
-                <h5 class="w3-opacity"><b><?php echo $row["profile_name"];?></b></h5>
-                <h6 class="w3-text-teal"> <span class="w3-tag w3-teal w3-round"><?php echo $row["duration"];?></span>
-                    <?php if($row['status'])
-                    echo '- <span class="w3-tag w3-teal w3-round"> Current </span>';
-                    ?>
-                </h6>
-                <p>Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.</p>
-                <hr>
-              </div>
-         <?php }
-          ?>
-        </div>
-
-
-      <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>W3Schools.com</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
-          <p>Web Development! All I need to know in one place</p>
-          <hr>
-        </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>London Business School</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
-          <p>Master Degree</p>
-          <hr>
-        </div>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>School of Coding</b></h5>
-          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
-          <p>Bachelor Degree</p><br>
-        </div>
-      </div>
-
-
+          $sql="INSERT INTO `experience` (`id`, `profile_name`, `duration`, `status`, `description`) VALUES (NULL, '$profile_name', '$duration','$description')";
+          if(mysqli_query($conn, $sql)){
+            echo"<script>window.location='index.php'; </script>";
+        }
+      }
+    ?>
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Add Your Experience</h2>
+            <form class="w3-container w3-card-4" action="" method='post'>
+            
+               
+                <p>      
+                  <label class="w3-text-blue"><b>Profilename</b></label>
+                  <input class="w3-input w3-border" name="p-name" type="text">
+                </p>
+                <p>      
+                  <label class="w3-text-blue"><b>Duration</b></label>
+                  <input class="w3-input w3-border" name="du-tion" type="text">
+                </p>
+                <p>      
+                  <label class="w3-text-blue"><b>Description</b></label>
+                  <input class="w3-input w3-border" name="dis-tion" type="text">
+                </p>
+                <p>      
+                  <button class="w3-btn w3-blue" name="addwork">Add</button>
+                </p>
+            </form>
+       </div>
 
 
     <!-- End Right Column -->
